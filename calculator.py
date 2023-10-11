ecf69ad629040e8cc55b155da280f9dfb5f7cff0
GabCoolGuy <GabCoolDude@users.noreply.github.com>
2023-10-11 18:09:40 +0200

    Fix decimal numbers not working

--- calculator.py
+++ calculator.py
@@ -2,12 +2,12 @@ sign = input("What do you want to do? +, -, /, or *: ")
 num1 = input("Enter a number: ")
 num2 = input("Enter a second number: ")
 if sign == "+":
-    result = int(num1) + int(num2)
+    result = float(num1) + float(num2)
 if sign == "-":
-    result = int(num1) - int(num2)
+    result = float(num1) - float(num2)
 if sign == "*":
-    result = int(num1) *  int(num2)
+    result = float(num1) *  float(num2)
 if sign == "/":
-    result = int(num1) / int(num2)
+    result = float(num1) / float(num2)
 
-print(int(result))
+print(result)
