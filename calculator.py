e1d26dc80a196839e2b860d28d8ce7aec3202e38
GabCoolGuy <GabCoolDude@users.noreply.github.com>
2023-10-11 17:41:11 +0200

    Add substract

--- calculator.py
+++ calculator.py
@@ -1,7 +1,11 @@
 from math import *
 
+sign = input("What do you want to do? +, -, /, or *: ")
 num1 = input("Enter a number: ")
 num2 = input("Enter a second number: ")
+if sign == "+":
+    result = int(num1) + int(num2)
+if sign == "-":
+    result = int(num1) - int(num2)
 
-result = int(num1) + int(num2)
 print(result)
